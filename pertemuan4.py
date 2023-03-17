12a359ad7d3d3b15fbdee7bea78c521a73c015fd
auliadivaw <auliarahmadivaw@gmail.com>
2023-03-17 09:02:21 +0700

    file belum dimodifikasi

---
+++ pertemuan4.py
@@ -0,0 +1,55 @@
+from abc import ABCMeta, abstractmethod 
+
+    # kelas abstrak
+class DuaDimensi(metaclass=ABCMeta):
+    @abstractmethod
+    def luas(self):
+        pass
+
+# kelas Segiempat turunan dari kelas Dua Dimensi 
+class Segiempat(DuaDimensi):
+    def __init__(self, p, l):
+        self.panjang = p
+        self.lebar = l
+    # mengimplementasikan metode luas ()
+    def luas(self):
+        return self.panjang * self.lebar
+
+# kelas Segitiga turunan dari kelas DuaDimensi 
+class Segitiga(DuaDimensi):
+    def __init__ (self, a, t):
+        self.alas = a
+        self.tinggi = t
+        #mengimplementasikan metode luas ()
+    def luas (self):
+        return (self.alas * self.tinggi) / 2
+
+# kelas Lingkaran turunan dari kelas DuaDimensi 
+class Lingkaran(DuaDimensi):
+    def __init__ (self, r):
+        self.radius = r
+        # mengimplementasikan metode luas ()
+    def luas(self):
+        import math
+        return math.pi* (self.radius ** 2)
+# membuat list kosong
+mylist = []
+print(mylist)
+ 
+mylist.append(Segiempat(10, 8))
+print(mylist)
+mylist.append(Segitiga(3, 5))
+print(mylist)
+mylist.append(Lingkaran(4))
+print(mylist)
+
+for obj in mylist:
+    if not issubclass (obj.__class__, DuaDimensi):
+        raise TypeError('Objek harus turunan dari Dua Dimensi')
+    if isinstance (obj, Segiempat):
+        print ('Luas segiempat = ', end='')
+    elif isinstance (obj, Segitiga):
+        print ('Luas segiempat = ', end='')
+    else: 
+        print ('Luas lingkaran = ', end='')
+    print(obj.luas())
